0331aa625aa2644fbc7174b81a4fd70b90181e97
vivekpatil37 <109573095+vivekpatil37@users.noreply.github.com>
2022-08-16 10:24:07 +0530

    Add files via upload

---
+++ Arithmetic_Operation.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+{
+  int num1=4,num2=2,sum=0,sub=0,mul=0,div=0,mod=0;
+  sum=num1+num2;
+  printf("Addition is=%d\n",sum);
+
+  sub=num1-num2;
+  printf("Substraction is=%d\n",sub);
+
+  mul=num1*num2;
+  printf("Multiplication is=%d\n",mul);
+
+  div=num1/num2;
+  printf("Division is=%d\n",div);
+
+  mod=num1%num2;
+  printf("Modulo is=%d\n\n",mod);
+
+  printf("Thank you!!!");
+
+}
