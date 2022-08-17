23406e1c2df075696d4d9caed0ee4a27f00c9afe
vivekpatil37 <109573095+vivekpatil37@users.noreply.github.com>
2022-08-17 17:54:19 +0530

    Add files via upload

---
+++ Addition of two numbers .c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{ 
+  int num1=0,num2=0,sum=0;
+  
+ printf("Enter the first number=");
+ scanf("%d",&num1);
+
+ printf("Enter the second number=");
+ scanf("%d",&num2);
+
+ sum=num1+num2;
+ printf("Addition is=%d",sum);
+}
