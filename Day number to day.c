38207e8fea37d047b83501d9f69d320c5bf1a563
vivekpatil37 <109573095+vivekpatil37@users.noreply.github.com>
2022-08-19 17:30:40 +0530

    Add files via upload

---
+++ Day number to day.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Day=0;
+    printf("Enter day number=");
+    scanf("%d",&Day);
+
+    if(Day==1)
+    {
+        printf("Monday");
+    }
+    if(Day==2)
+    {
+        printf("Tuesday");
+    }
+    if(Day==3)
+    {
+        printf("Wednsday");
+    }
+    if(Day==4)
+    {
+        printf("Thursday");
+    }
+    if(Day==5)
+    {
+        printf("Friday");
+    }
+    if(Day==6)
+    {
+        printf("Saturday");
+    }
+    if(Day==7)
+    {
+        printf("Sunday");
+    }
+
+    printf("\nThank you!!!");
+}
